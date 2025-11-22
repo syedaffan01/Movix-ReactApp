c07b5f5fd013ba40d08dfc7d333ae0ca00b0ba9e
syedaffan01 <affan4157@gmail.com>
2025-11-22 22:47:28 +0530

    styling home..

---
+++ react-app/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+const App = () => {
+
+    return (
+        <main>
+            <div className="pattern"/>
+
+            <div className="wrapper">
+                <header>
+                    <img src="/hero.png" alt="Hero Banner" />
+                    <h1>Find <span className="text-gradient">Movies</span>  You'll Enjoy Without Hassle</h1>
+                </header>
+
+                <p>Search</p>
+
+
+                </div>
+
+
+
+
+        </main>
+    )
+}
+
+
+export default App
