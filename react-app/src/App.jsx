7976faa498a5659f64349531c003fa6d40d76f1f
syedaffan01 <affan4157@gmail.com>
2025-12-10 05:29:06 +0530

    ..

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,68 @@
 
 import './App.css'
 import Search from "./components/Search.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import Spinner from "./components/Spinner.jsx";
+
+const API_BASE_URL='https://api.themoviedb.org/3'
+
+const API_KEY= import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+    methods: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+    }
+
+
+}
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('');
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [movieList, setMovieList] = useState([]);
+
+    const  fetchMovies = async () => {
+        setIsLoading(true);
+        setErrorMessage('');
+
+
+        try{
+const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+
+            const response = await fetch(endpoint, API_OPTIONS)
+
+            if(!response.ok){
+                throw new Error('Failed to fetch movies')
+            }
+            const data = await response.json()
+
+            if(data.Response === 'False'){
+                setErrorMessage(data.Error || 'Failed to fetch movies')
+                setMovieList([]);
+                return
+            }
+
+            setMovieList(data.results)
+
+        }catch (error) {
+            console.log(`Error fetching movies: ${error}`)
+            setErrorMessage('Error fetching movies')
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+fetchMovies();
+    }, [])
+
+
 
     return (
         <main>
@@ -14,9 +72,31 @@ const App = () => {
                 <header>
                     <img src="/hero.png" alt="Hero Banner" />
                     <h1>Find <span className="text-gradient">Movies</span>  You'll Enjoy Without Hassle</h1>
+
+                    <Search searchTerm={searchTerm } setSearchTerm={setSearchTerm} />
                 </header>
 
-                <Search searchTerm={searchTerm } setSearchTerm={setSearchTerm} />
+                <section className="all-movies">
+                    <h2 className="mt-[40px]">All Movies</h2>
+
+
+                    {isLoading ? (
+                       <Spinner />
+                    ): errorMessage ? (
+                        <p className="text-red-500">{errorMessage}</p>
+                    ) : (
+                        <ul>
+                            {movieList.map(movie => (
+                                <p key={movie.id} className="text-white">{movie.title}</p>
+                            ))}
+                        </ul>
+                    )}
+
+
+
+                </section>
+
+
                 <h1 className="text-white">{searchTerm}</h1>
 
 
